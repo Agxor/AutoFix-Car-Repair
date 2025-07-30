abbb751a1780fc51166e37fbf1f6d33ee8636bde
Agxor <chakrabortyanurag445@gmail.com>
2025-07-31 00:14:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Optional: Add smooth scroll or animation
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+
+
+
+
+
+
+
